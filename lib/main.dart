0e11769f23f2d9d136b44b92dd1b7ad2b65d3edd
Md. Obaidul Islam <obdcse360@gmail.com>
2025-12-13 01:01:50 +0600

    theme dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:notes/src/all_file/all_provider/dark_provider.dart';
 import 'package:notes/src/all_file/all_provider/note_provider.dart';
 import 'package:notes/src/all_file/splash_ui.dart';
 import 'package:provider/provider.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MultiProvider(
@@ -22,6 +23,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    return SafeArea(
+      child: ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Dark Mode Switch',
+            theme: ThemeData.light(),
+            darkTheme: ThemeData.dark(),
+            themeMode: themeProvider.isDarkMode
+                ? ThemeMode.dark
+                : ThemeMode.light,
+            home: SplashScreen(), //SplashScreen(),
+          );
+        },
+      ),
+    );
   }
 }
